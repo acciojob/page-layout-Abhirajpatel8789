df35f05600e32faaafa2620863c0a061e90fa06d
Your Name <your@email.com>
2026-04-05 13:31:33 +0530

    Third attemot

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const PageLayout = (props) => {
   return (
-    <div>
+    <>
       <h1>{props.header}</h1>
       <p>{props.children}</p>
       <footer>{props.footer}</footer>
-    </div>
+    </>
   )
 }
 
